e551756436389810aed9bd00d521a3ffdf988ba3
MakerSquare Student <students@themakersquare.com>
2013-09-10 17:33:21 -0500

    Fill in person class

--- powerranger.rb
+++ powerranger.rb
@@ -1,8 +1,37 @@
 class Person
+	attr_accessor :name, :caffeine_level
+
+	def run
+		puts "  O    "
+		puts " -|--  "
+		puts "  |    "
+		puts " / |   "
+		puts "/   |  "
+		
+	end
+
+	def scream
+		puts "AAAAAAAARRRRRRHHHHHHHHHH!!!!"
+		
+	end
+
+	def drink_coffee
+		puts "  ( (     "
+		puts "   ) )    "
+		puts " ------   "
+		puts "|      | ]"
+		puts " \\    /  "
+		puts "  ----   "
+		
+	end
 end
 
-class PowerRanger
+class PowerRanger < Person
+	
 end
 
 class EvilNinja
 end
+
+bob = Person.new()
+bob.drink_coffee
